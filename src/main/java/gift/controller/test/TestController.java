b019d09a2b8102b7940c8d7f93a7811abd0bb203
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-08-09 16:31:32 +0900

    test

--- src/main/java/gift/controller/test/TestController.java
+++ src/main/java/gift/controller/test/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
+   /* @GetMapping("/test")
     public String test() {
         return "Hello!";
-    }
+    }*/
 }
